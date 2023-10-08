c17f2663f43fff7a3021836849b74bebb4ba0070
atrgouti <bilalatrgouti2002@gmail.com>
2023-10-09 00:58:15 +0200

    added new products

--- src/pages/homeComponents/ProductsQuickView.jsx
+++ src/pages/homeComponents/ProductsQuickView.jsx
@@ -2,7 +2,16 @@ import styles from "./ProductsQuickView.module.css";
 import ShortLine from "../../components/ShoerLine";
 import data from "../../data/data.json";
 import HomeProduct from "./HomeProduct";
+// react crousal
 import Carousel from "@itseasy21/react-elastic-carousel";
+// font awsome package
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+//import hooks
 import { useState, useRef } from "react";
 
 function ProductsQuickView() {
@@ -63,6 +72,27 @@ function ProductsQuickView() {
             pagination={false}
             showArrows={true}
             ref={carouselRef} // Assign the ref to the Carousel component
+            renderArrow={({ type, onClick }) => (
+              <button
+                className={styles.customArrow} // Add a custom CSS class
+                onClick={onClick}
+              >
+                {type === "PREV" ? (
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronRight} />
+                )}
+              </button>
+            )}
+            breakPoints={[
+              {
+                width: 1,
+                itemsToShow: 1,
+                itemsToScroll: 1,
+              }, // Screens less than 600px
+              { width: 600, itemsToShow: 2, itemsToScroll: 1 }, // Screens between 600px and 900px
+              { width: 900, itemsToShow: 3, itemsToScroll: 1 }, // Screens wider than 900px
+            ]}
           >
             {data.mibidata[choosedCategory].map((product) => (
               <HomeProduct
