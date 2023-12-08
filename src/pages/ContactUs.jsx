cd4f64da53770e469254ba0a08db0b4ccc37f52d
atrgouti <bilalatrgouti2002@gmail.com>
2023-12-08 01:51:49 +0100

    update

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,6 +4,22 @@ import filtringStyles from "./filtring.module.css";
 import CurrentCategory from "../components/CurrentCategory";
 import ContactUsForm from "./ContactUsForm";
 import Footer from "../components/Footer";
+import styles from "./contactUs.module.css";
+
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+
+// fontawsome
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faPinterest,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 function ContactUs({
   ActiveFilter,
@@ -15,6 +31,63 @@ function ContactUs({
   increaseQuantity,
   decreaseQuntity,
 }) {
+  // window.screenTop({ top: 0 });
+  window.scrollTo({ top: 0 });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function handleSubmitForm(e) {
+    e.preventDefault();
+    if (!email || !number || !name || !subject || !message)
+      return toast.error("Please fill all the feilds", {
+        position: "bottom-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+
+    if (email.indexOf("@") === -1)
+      return toast.error("Please write a valid email", {
+        position: "bottom-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    try {
+      const { data } = await axios.post("http://localhost:4000/api/mail", {
+        name,
+        email,
+        number,
+        subject,
+        message,
+      });
+      setName("");
+      console.log("data has been sent succefully", data);
+      toast.success("Your message has been sent sucefully🎉", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
   return (
     <>
       <div
@@ -36,15 +109,32 @@ function ContactUs({
         style={{ position: "relative", top: "110px", backgroundColor: "white" }}
       >
         <CurrentCategory category={"Contact Us"} />
-        <div
-          style={{
-            width: "80%",
-            margin: "50px auto",
-            display: "flex",
-            gap: "15px",
-          }}
-        >
-          <div style={{ width: "65%" }}>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        <ToastContainer
+          position="top-center"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        <div className={styles.formArea}>
+          <div className={styles.formDetailsleft}>
             <h2 style={{ textAlign: "center" }}>Contact Us</h2>
             <p
               style={{ textAlign: "center", color: "gray", marginTop: "10px" }}
@@ -53,9 +143,52 @@ function ContactUs({
               aren’t sure who to contact? Get in touch and a member of our team
               will reach out to you.
             </p>
-            <ContactUsForm />
+
+            <ContactUsForm
+              handleSubmitForm={handleSubmitForm}
+              setName={setName}
+              setEmail={setEmail}
+              setNumber={setNumber}
+              setSubject={setSubject}
+              setMessage={setMessage}
+            />
           </div>
-          <div style={{ width: "35%", backgroundColor: "pink" }}></div>
+          <div className={styles.rightDetails}>
+            <div className={styles.adress}>
+              <p style={{ fontWeight: "bold" }}>ADRESS:</p>
+              <p>55 Gallaxy Enque, 2568 steet, 23568 NY</p>
+              <p>PHONE: +1 (440) 568 4568</p>
+              <p>EMAIL: sales@yousite.com</p>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.opningHours}>
+              <p style={{ fontWeight: "bold" }}>OPENING HOURS</p>
+              <p>Mon - Sat : 9am - 11pm</p>
+              <p>Sunday: 11am - 5pm</p>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.stayConnected}>
+              <p style={{ fontWeight: "bold" }}>STAY CONNECTED</p>
+              <div className={styles.icons}>
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className={styles.socialIcon}
+                />
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className={styles.socialIcon}
+                />
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className={styles.socialIcon}
+                />
+                <FontAwesomeIcon
+                  icon={faPinterest}
+                  className={styles.socialIcon}
+                />
+              </div>
+            </div>
+          </div>
           <div></div>
         </div>
         <Footer />
--- src/pages/ContactUsForm.jsx
+++ src/pages/ContactUsForm.jsx
@@ -1,71 +1,57 @@
 import { useState } from "react";
+
 import styles from "./contactUs.module.css";
-import axios from "axios";
-function ContactUsForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [number, setNumber] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
 
-  async function handleSubmitForm(e) {
-    e.preventDefault();
-    if (!email || !number || !name || !subject || !message)
-      return console.log("fill all the feilds");
-    console.log(email, name);
-    try {
-      const { data } = await axios.post("http://localhost:4000/api/mail", {
-        name,
-        email,
-        number,
-        subject,
-        message,
-      });
-      console.log("data has been sent succefully", data);
-    } catch (error) {
-      console.log("error:", error);
-    }
-  }
+function ContactUsForm({
+  handleSubmitForm,
+  setEmail,
+  setMessage,
+  setName,
+  setNumber,
+  setSubject,
+}) {
   return (
-    <form action="">
-      <div className={styles.nameEmail}>
-        <input
-          type="text"
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        />
+    <>
+      <form action="">
+        <div className={styles.nameEmail}>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
 
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
         <input
+          onChange={(e) => setNumber(e.target.value)}
+          placeholder="Phone Number"
+          className={styles.phoneNumber}
           type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-      <input
-        onChange={(e) => setNumber(e.target.value)}
-        placeholder="Phone Number"
-        className={styles.phoneNumber}
-        type="text"
-      />
-      <input
-        onChange={(e) => setSubject(e.target.value)}
-        placeholder="Subject"
-        className={styles.subject}
-        type="text"
-      />
-      <textarea
-        className={styles.textArea}
-        name=""
-        id=""
-        cols="30"
-        rows="10"
-        placeholder="Message"
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-      <button onClick={(e) => handleSubmitForm(e)} className={styles.submit}>
-        Send
-      </button>
-    </form>
+        <input
+          onChange={(e) => setSubject(e.target.value)}
+          placeholder="Subject"
+          className={styles.subject}
+          type="text"
+        />
+        <textarea
+          className={styles.textArea}
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Message"
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button onClick={(e) => handleSubmitForm(e)} className={styles.submit}>
+          Send
+        </button>
+      </form>
+    </>
   );
 }
 
